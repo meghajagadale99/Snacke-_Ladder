9e8344e1abc7220f9b61eb3bf79cf47ba9dd5a77
Megha <meghajagadale99@gmail.com>
2021-08-07 01:06:34 +0530

    Added: starting postion

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -2,6 +2,9 @@ package com.bridgelabz;
 
 public class SnakeLadder {
     public static void main(String[] args) {
+        //variables
+         int playerPosition=0;
         System.out.println("Welcome to the Snake and Ladder Game");
+        System.out.println(" Single Player starts at Position: "+playerPosition);
     }
 }
