f26134d24ac46db7dca23a7a00891bed284f6921
Megha <meghajagadale99@gmail.com>
2021-08-07 01:22:35 +0530

    Added: cheking randome numbers of a die

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,10 +1,13 @@
 package com.bridgelabz;
 
-public class SnakeLadder {
-    public static void main(String[] args) {
+public class SnakeLadder
+{
+    public static void main(String[] args)
+    {
         //variables
          int playerPosition=0;
         System.out.println("Welcome to the Snake and Ladder Game");
-        System.out.println(" Single Player starts at Position: "+playerPosition);
+        int rollDice= ((int) (Math.floor(Math.random() * 10)) % 6) + 1;
+        System.out.println("Player rolled dice value : "+rollDice);
     }
 }
