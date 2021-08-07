ff52babf7e85b5cf3dfb78ebd1dd8df44f8725aa
Megha <meghajagadale99@gmail.com>
2021-08-07 14:38:25 +0530

    Added: Repeat till the Player reaches

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,39 +1,38 @@
 package com.bridgelabz;
 
-public class SnakeLadder
-{
+public class SnakeLadder {
+    public static final int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+    public static final int WINNING_POSITION = 100;
 
-    public static final int NO_PLAY=0,LADDER=1,SNAKE=2;
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game");
         //variables
-        int playerPosition=0;
-        System.out.println("Player starts at Position: "+playerPosition);
+        int playerPosition = 0;
+        while  (playerPosition < WINNING_POSITION)
+        {
+            int rollDice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            System.out.println("Player rolled dice value : " + rollDice);
 
-        int rollDice= ((int) (Math.floor(Math.random() * 10)) % 6) + 1;
-        System.out.println("Player rolled dice value : "+rollDice);
-
-        int Option= (int) (Math.floor(Math.random() * 10) % 3);
-        switch (Option) {
-            case NO_PLAY:
-                System.out.println("No Play");
-                playerPosition += 0;
-                break;
-            case LADDER:
-                System.out.println("Ladder");
-                playerPosition += rollDice;
-                break;
-            case SNAKE:
-                System.out.println("Snake");
-                playerPosition -= rollDice;
-                break;
-            default:
-                System.out.println("Nothing");
-                break;
-
+            int Option = (int) (Math.floor(Math.random() * 10) % 3);
+            switch (Option) {
+                case NO_PLAY:
+                    System.out.println("No Play");
+                    break;
+                case LADDER:
+                    System.out.println("Ladder");
+                    playerPosition += rollDice;
+                    break;
+                case SNAKE:
+                    System.out.println("Snake");
+                    if(playerPosition > rollDice)
+                        playerPosition -= rollDice;
+                    else
+                        playerPosition=0;
+                    break;
+            }
+            System.out.println("The Player Position Is : " + playerPosition);
         }
-        System.out.println("The Player Position Is : "+playerPosition);
+        System.out.println( "Player  Wins  The  Game" );
 
     }
 }
